37bdc0c6a707e0ffdb6bb5a20c0bef5050bdbf1f
Gabriel Bauer <gabeb1277@gmail.com>
2025-04-28 10:35:51 -0400

    Py 101 - read a file?

--- pre.py
+++ pre.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+# Open the file in read mode
+file = open("grub", "r")
+
+# Read each line one by one
+for line in file:
+    print(line.strip())  # .strip() to remove newline characters
+
+# Close the file
+file.close()
